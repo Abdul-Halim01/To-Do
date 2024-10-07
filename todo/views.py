c5309ff81054e4be7c0d7bbe2479727ebbaaf593
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2024-10-07 23:17:43 +0300

    new updates

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
-
+from todo_app.models import Task
 def home(request):
-    return render(request,'home-todo.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context= {
+        'tasks':tasks,
+    }
+    print(tasks)
+    return render(request,'home-todo.html',context)
--- todo_app/admin.py
+++ todo_app/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from todo_app.models import Task
 
 # Register your models here.
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display=('task','is_completed','updated_at')
+    search_fields = ['task']
+
+
+admin.site.register(Task,TaskAdmin)
